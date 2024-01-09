e62db260fd02d9a07afa056ffacd8b54eacaa1e4
udezueoluomachi <74429592+udezueoluomachi@users.noreply.github.com>
2024-01-09 23:10:36 +0100

    fix

--- src/routes/webhook.js
+++ src/routes/webhook.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const {validatePaystackWebhook} = require("../middleware/validators");
 const recordPaystackMessage = require("../middleware/paystackWebhookRecorder")
-const controller = require("../controller/webhook")
+const controller = require("../controllers/webhook")
 
 router.post("/paystack", validatePaystackWebhook, recordPaystackMessage, controller.paystackEvents.middleware)
 
